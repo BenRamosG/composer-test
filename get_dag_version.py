3a867842b7103cb699a5e5a189bbd042479ef144
BenRamosG <153843795+BenRamosG@users.noreply.github.com>
2025-03-10 15:37:56 -0600

    Create get_dag_version.py

---
+++ get_dag_version.py
@@ -0,0 +1,39 @@
+import sys
+import re
+
+def get_dag_version(file_path, variable_name):
+
+    try:
+
+        with open(file_path, 'r') as f:
+            content = f.read()
+
+            pattern = rf'{variable_name} = "(.*?)"'  # Using raw f-string for regex
+            match = re.search(pattern, content)
+
+            if match:
+                return match.group(1)
+            else:
+                print(2)
+                return None  # Variable not found
+
+    except FileNotFoundError:
+        print(f"Error: File not found at {file_path}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+            
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python get_dag_version.py <filepath> <variablename>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    name_var = sys.argv[2]
+
+    result = get_dag_version(file_path, name_var)
+
+    if result is not None:
+        print(result)
