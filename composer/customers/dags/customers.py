818c8ca58c02db40166fb918364614d24e840fb3
napolitanog <napolitanog@google.com>
2025-03-13 10:33:27 -0300

    Test undoing changes in customers.py

--- composer/customers/dags/customers.py
+++ composer/customers/dags/customers.py
@@ -21,7 +21,7 @@ args = {
 
 
 
-with DAG("customers" + "_" +  dag_version,
+with DAG("customers",
          start_date= datetime(2025,3,8),
          schedule= "0 4 * * *",
          catchup= False,
