ef590d22bb14bffbc816c1c93e032bff36ea77e2
Benito Ramos <benitodaniel@google.com>
2025-03-14 17:23:29 +0000

    Add changes

--- tir-bigquery/01_tir_staging_dataset/01_tables/sql/StgProvider.sql
+++ tir-bigquery/01_tir_staging_dataset/01_tables/sql/StgProvider.sql
@@ -1,4 +1,4 @@
-CREATE or replace TABLE `telus-tir-project.dev_01_tir_staging_dataset.StgProvider`
+CREATE TABLE `telus-tir-project.dev_01_tir_staging_dataset.StgProvider`
 (
   Audit_Table STRING,
   max_id INT64
