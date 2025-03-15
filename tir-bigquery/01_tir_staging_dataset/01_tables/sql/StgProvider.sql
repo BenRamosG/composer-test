9bc8a47726d07e86c9f86f6f518dfd3192e07423
BenRamosG <153843795+BenRamosG@users.noreply.github.com>
2025-03-14 21:08:17 -0600

    Update StgProvider.sql

--- tir-bigquery/01_tir_staging_dataset/01_tables/sql/StgProvider.sql
+++ tir-bigquery/01_tir_staging_dataset/01_tables/sql/StgProvider.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `telus-tir-project.dev_01_tir_staging_dataset.StgProvider`
+CREATE OR REPLACE TABLE `telus-tir-project.dev_01_tir_staging_dataset.StgProvider`
 (
   Audit_Table STRING,
   max_id INT64
