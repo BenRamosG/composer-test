4dfc6f45017b06120d190c8387226457eb875cdf
BenRamosG <153843795+BenRamosG@users.noreply.github.com>
2025-03-20 22:15:28 -0600

    Update reference_load.py

--- composer/reference/dags/reference_load.py
+++ composer/reference/dags/reference_load.py
@@ -21,7 +21,7 @@ args = {
 
 
 
-with DAG(f"reference_process_{dag_version}",
+with DAG(f"reference_{dag_version}",
          start_date= datetime(2025,3,8),
          schedule= "0 4 * * *",
          catchup= False,
